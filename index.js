7b8b6bba5176f16907d5cb5d32b92e8eb184206e
Joachim Hahn <joachim.hahn@1und1.de>
2016-11-08 16:52:40 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var config = require('config');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello brave new World!');
+    res.send('Aloha from '+config.state);
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('dockerexpress 3000!');
+    console.dir(config);
 });
